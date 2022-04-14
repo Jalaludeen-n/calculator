51c6291326358b744b0f2df4992338f7e6d194e1
jalaludeen-n <connectdeen@gmail.com>
2022-04-14 06:12:33 +0530

    JSP version

--- calculator/src/main/java/com/example/calculator/CalculatorServlet.java
+++ calculator/src/main/java/com/example/calculator/CalculatorServlet.java
@@ -1,22 +1,11 @@
 package com.example.calculator;
 
-
-
 import java.io.IOException;
-
-
 import java.io.PrintWriter;
-
-
+import java.util.Stack;
 import javax.servlet.ServletException;
-
-
 import javax.servlet.http.HttpServlet;
-
-
 import javax.servlet.http.HttpServletRequest;
-
-
 import javax.servlet.http.HttpServletResponse;
 
 
@@ -25,24 +14,98 @@ public class CalculatorServlet extends HttpServlet {
 
     public void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-            response.setContentType("text/html;charset=UTF-8");
-            PrintWriter out = response.getWriter();
-            double n1 = 0;
-        request.setAttribute("name", n1);
-        request.getRequestDispatcher("calculator.jsp").forward(request, response);
-
-        n1 = Double.parseDouble(request.getParameter("txtN1"));
-
-
-//        double n2 = Double.parseDouble(request.getParameter("txtN2"));
-//        double result =0;
-//        String opr=request.getParameter("opr");
-//        if(opr.equals("+")) result=n1+n2;
-//        if(opr.equals("-")) result=n1-n2;
-//        if(opr.equals("*")) result=n1*n2;
-//        if(opr.equals("/")) result=n1/n2;
-
+            String result = "";
+            request.setAttribute("result", result);
+            request.getRequestDispatcher("calculator.jsp").forward(request, response);
 
     }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String result = request.getParameter("Input");
+        request.setAttribute("result", evaluate(result));
+        request.getRequestDispatcher("calculator.jsp").forward(request, response);
+
+    }
+    public int evaluate(String expression)
+    {
+        char[] tokens = expression.toCharArray();
+        Stack<Integer> values = new
+                Stack<Integer>();
+        Stack<Character> ops = new
+                Stack<Character>();
+
+        for (int i = 0; i < tokens.length; i++)
+        {
+            if (tokens[i] == ' ')
+                continue;
+            if (tokens[i] >= '0' &&
+                    tokens[i] <= '9')
+            {
+                StringBuffer sbuf = new
+                        StringBuffer();
+                while (i < tokens.length &&
+                        tokens[i] >= '0' &&
+                        tokens[i] <= '9')
+                    sbuf.append(tokens[i++]);
+                values.push(Integer.parseInt(sbuf.
+                        toString()));
+                i--;
+            }
+
+
+            else if (tokens[i] == '+' ||
+                    tokens[i] == '-' ||
+                    tokens[i] == '*' ||
+                    tokens[i] == '/')
+            {
+                while (!ops.empty() &&
+                        hasPrecedence(tokens[i],
+                                ops.peek()))
+                    values.push(applyOp(ops.pop(),
+                            values.pop(),
+                            values.pop()));
+
+                ops.push(tokens[i]);
+            }
+        }
+        while (!ops.empty())
+            values.push(applyOp(ops.pop(),
+                    values.pop(),
+                    values.pop()));
+        return values.pop();
+    }
+    public static boolean hasPrecedence(
+            char op1, char op2)
+    {
+        if (op2 == '(' || op2 == ')')
+            return false;
+        if ((op1 == '*' || op1 == '/') &&
+                (op2 == '+' || op2 == '-'))
+            return false;
+        else
+            return true;
+    }
+
+    public static int applyOp(char op,
+                              int b, int a)
+    {
+        switch (op)
+        {
+            case '+':
+                return a + b;
+            case '-':
+                return a - b;
+            case '*':
+                return a * b;
+            case '/':
+                if (b == 0)
+                    throw new
+                            UnsupportedOperationException(
+                            "Cannot divide by zero");
+                return a / b;
+        }
+        return 0;
+    }
+
 
 }
